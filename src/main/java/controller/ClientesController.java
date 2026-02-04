142d453e05730793d352ee4722fded274499d0b5
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-04 18:16:14 -0500

    refactor: completed methods

--- src/main/java/controller/ClientesController.java
+++ src/main/java/controller/ClientesController.java
@@ -2,9 +2,10 @@ package controller;
 
 import model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import service.ClientesService;
 
 @RestController
@@ -12,7 +13,16 @@ public class ClientesController {
     @Autowired
     ClientesService clientesService;
     @GetMapping(value="autenticar", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Cliente autenticar(String usuario, String password){
+    public Cliente autenticar(@RequestParam("usuario") String usuario,@RequestParam("password") String password){
         return clientesService.autenticarCliente(usuario, password);
+    }
+
+    @PostMapping(value="registrar", consumes =  MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> registrar(@RequestBody Cliente cliente){
+        if(clientesService.registrarCliente(cliente)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 }
