82211db83727fcb856a6b3d301b0ace22352592b
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-07 11:31:22 -0500

    fix: permitir peticiones globales

--- src/main/java/controller/ClientesController.java
+++ src/main/java/controller/ClientesController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.ClientesService;
 
+@CrossOrigin("*")
 @RestController
 public class ClientesController {
     @Autowired
